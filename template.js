45b20a5065871909b6b5ca98fab1297aa657120b
John Pavley <john@pavley.com>
2021-10-15 23:23:24 -0400

    saved copy of code as template

---
+++ template.js
@@ -0,0 +1,73 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particleArray = [];
+
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;ctx.fillStyle = 'white';
+});
+
+const mouse = {
+    x: undefined,
+    y: undefined
+};
+
+canvas.addEventListener('click', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+canvas.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+class Particle {
+    constructor() {
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1; // random number between 1 and 6
+        this.speedX = Math.random() * 3 - 1.5; // random number between -1.5 and +1.5
+        this.speedY = Math.random() * 3 - 1.5; // random number between -1.5 and +1.5
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function init() {
+    for (let i = 0; i < 100; i++) {
+        particleArray.push(new Particle());
+    }
+}
+
+function handleParticles() {
+    for (let i = 0; i < particleArray.length; i++) {
+        particleArray[i].update();
+        particleArray[i].draw();
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
